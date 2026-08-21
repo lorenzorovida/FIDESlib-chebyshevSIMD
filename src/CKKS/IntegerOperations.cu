514ee44cd2298b9a61decbc8de47a57f03c7191e
Lorenzo Rovida <lorenzo.rovida@outlook.it>
2026-08-21 14:36:45 +0200

    Update IntegerOperations.cu

--- src/CKKS/IntegerOperations.cu
+++ src/CKKS/IntegerOperations.cu
@@ -1024,14 +1024,19 @@ void csa4(Ciphertext& out, const Ciphertext& a, const Ciphertext& b, const Ciphe
 
 	csa3(s1, c1, a, b, c);
 
-	out.copy(c1);
+	out.copy(s1);
 	return;
+
+	//c1 è sbagliato
+	//out.copy(c1);
+	//return;
 
 
 	// c1 = rot(c1, -1)
 	Ciphertext c1_rot(a.cc_);
-
 	c1_rot.rotate(c1, -1);
+
+	//c1 rot is broken it seems
 
 	
 
