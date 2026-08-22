7b1553b4d18ec1984d867e19b1cd50388d811a4f
Lorenzo Rovida <lorenzo.rovida@outlook.it>
2026-08-22 19:54:57 +0200

    Update IntegerOperations.cu

--- src/CKKS/IntegerOperations.cu
+++ src/CKKS/IntegerOperations.cu
@@ -461,6 +461,9 @@ void evalIntegerMult(Ciphertext& out,
 		//     overflow);
 
 		evalIntegerMult(result, a, b, bits / 2, bits_original, 4 * repetitions, repetitions_original, overflow, coeffs, cc);
+
+		out.copy(result);
+		return;
 	}
 
 	// ============================================================
@@ -635,6 +638,7 @@ void evalIntegerMult(Ciphertext& out,
 		}
 
 		BootstrapStCFirstBits(result, result.slots, false);
+		
 	}
 
 	out.copy(result);
