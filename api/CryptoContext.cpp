ad5da1582fc70e84eb805c67694a2ad42c293bc1
Lorenzo Rovida <lorenzo.rovida@outlook.it>
2026-08-24 11:04:53 +0200

    aa

--- api/CryptoContext.cpp
+++ api/CryptoContext.cpp
@@ -1101,7 +1101,7 @@ void CryptoContextImpl<DCRTPoly>::ProcessArrayPrecomputations(const Ciphertext<D
 	std::cout << "Done preprocessing with " << bits << " bits! " << std::endl;
 }
 
-void CryptoContextImpl<DCRTPoly>::ProcessMultiplications(std::vector<std::vector<double>> coeffs, FIDESlib::CKKS::Ciphertext) {
+void CryptoContextImpl<DCRTPoly>::ProcessMultiplications(std::vector<std::vector<double>> coeffs, FIDESlib::CKKS::Ciphertext c) {
 	FIDESlib::CudaNvtxRange r("API");
 	if (this->devices.empty()) {
 
