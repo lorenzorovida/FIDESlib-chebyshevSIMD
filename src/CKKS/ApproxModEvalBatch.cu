5ac6d530641fc7f214607d2bea9eca157e35aeff
Lorenzo Rovida <lorenzo.rovida@outlook.it>
2026-08-24 12:45:31 +0200

    Update ApproxModEvalBatch.cu

--- src/CKKS/ApproxModEvalBatch.cu
+++ src/CKKS/ApproxModEvalBatch.cu
@@ -121,7 +121,7 @@ const Plaintext* makePerSlotPlaintext(lbcrypto::CryptoContext<lbcrypto::DCRTPoly
 		// Replay: zero-copy, return a pointer straight into the cache.
 		return &cache->next();
 	} else {
-		std::cout << "Non sto usando la cache!" << std::endl;
+		std::cout << "Non sto usando la cache! " << cache << std::endl;
 	}
 
 	uint32_t openfheLevel = static_cast<uint32_t>(like.cc.L - like.getLevel());
