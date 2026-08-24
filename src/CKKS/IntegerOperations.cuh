e65c7b5f90184c6a9d600f251c5082833f0b77ad
Lorenzo Rovida <lorenzo.rovida@outlook.it>
2026-08-24 10:57:33 +0200

    a

--- src/CKKS/IntegerOperations.cuh
+++ src/CKKS/IntegerOperations.cuh
@@ -70,7 +70,7 @@ void preprocessProcessArray(int bits,
   FIDESlib::CKKS::Context& cc_,
   bool forB = false);
 
-void preprocessChebyshevMultiplication(std::vector<double> coeffs, lbcrypto::CryptoContext<lbcrypto::DCRTPoly>& cc);
+void preprocessChebyshevMultiplication(std::vector<std::vector<double>> coeffs, lbcrypto::CryptoContext<lbcrypto::DCRTPoly>& cc);
 
 void processArray(Ciphertext& out, const Ciphertext& c, const ProcessArrayPrecomputation& precomp);
 
