2dc6ba47f8f3fdc9b7ac100e3eea674120562ee3
Lorenzo Rovida <lorenzo.rovida@outlook.it>
2026-08-27 14:47:06 +0200

    Update IntegerOperations.cu

--- src/CKKS/IntegerOperations.cu
+++ src/CKKS/IntegerOperations.cu
@@ -1509,9 +1509,11 @@ void multiplier4bits(Ciphertext& result, Ciphertext& ctxtA, Ciphertext& ctxtB, i
 
 	//QUA RESULT è GIUSTO
 	
+		return;
+		
 	evalChebyshevSeriesPSBatch(cc, result, coeffs4BitsMultiplier, -1, 1);
 
-	return;
+
 
 	// evalChebyshevSeriesPSBatchApply(cc, result, precomp4bits, coeffs, -1, 1);
 	// evalChebyshevSeriesPSBatchRepeated(cc, result, coeffs, -1, 1);
