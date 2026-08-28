592ba6f91447e828487f35a5040609d4f75b1e17
Lorenzo Rovida <lorenzo.rovida@outlook.it>
2026-08-28 12:33:27 +0200

    Update CryptoContext.hpp

--- api/CryptoContext.hpp
+++ api/CryptoContext.hpp
@@ -212,7 +212,7 @@ template <> class CryptoContextImpl<DCRTPoly> {
 	Ciphertext<DCRTPoly> EvalAddInteger(const Ciphertext<DCRTPoly>& ct1, const Ciphertext<DCRTPoly>& ct2, int bits);
 	Ciphertext<DCRTPoly> EvalEqualInteger(const Ciphertext<DCRTPoly>& ct1, const Ciphertext<DCRTPoly>& ct2, int bits, int zslots, std::vector<double> coeffsSinc, int depth);
 	Ciphertext<DCRTPoly> EvalMultInteger(const Ciphertext<DCRTPoly>& ct1, const Ciphertext<DCRTPoly>& ct2, int bits, int zslots, bool overflow);
-	Ciphertext<DCRTPoly> EvalMultDivision(const Ciphertext<DCRTPoly>& ct1, const Ciphertext<DCRTPoly>& ct2, int bits, int zslots, bool overflow);
+	Ciphertext<DCRTPoly> EvalMultDivision(const Ciphertext<DCRTPoly>& ct1, const Ciphertext<DCRTPoly>& ct2, int bits, int zslots);
 
 	
 	// Support
